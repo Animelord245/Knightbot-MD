2b2f227a5ecfbb84404837f0efb4ca74fb92b19b
Animelord245 <eniobanke245@gmail.com>
2025-08-10 16:57:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Professor', // Your name Storm
+  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space 2348059389255
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
